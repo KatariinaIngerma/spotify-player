54504593244e0beec8a1051810594ee268e1f3fd
KatariinaIngerma <Katariina.ingerma@gmail.com>
2024-02-26 22:43:49 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './components/Home';
 import Search from './components/Search';
@@ -8,13 +8,13 @@ import Playlist from './components/Playlist';
 
 function App() {
   return (
-    <Route>
-    <Switch>
+    <Router>
+    <Routes>
       <Route exact path="/" component={Home} />
       <Route path="/search" component={Search} />
       <Route path="/playlist/:id" component={Playlist} />
-    </Switch>
-  </Route>
+    </Routes>
+  </Router>
   );
 }
 
