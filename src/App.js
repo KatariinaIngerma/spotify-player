4111edce7160519ffa78bf47ce49931ff8837755
KatariinaIngerma <Katariina.ingerma@gmail.com>
2024-02-27 15:45:04 +0200

    search function and login

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import Home from './components/Home';
-import Search from './components/Search';
-import Playlist from './components/Playlist';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Searcher from "./components/Searcher";
 
 function App() {
+  const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+  const REDIRECT_URI = process.env.REACT_APP_SPOTIFY_REDIRECT_URI;
+  const AUTH_ENDPOINT = process.env.REACT_APP_SPOTIFY_AUTH_ENDPOINT;
+  const RESPONSE_TYPE = process.env.REACT_APP_SPOTIFY_RESPONSE_TYPE;
+  const [token, setToken] = useState("");
+
+  console.log(CLIENT_ID);
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (hash && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"))
+        .split("=")[1];
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+
+    setToken(token);
+  }, []);
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
+
   return (
-    <Router>
-    <Routes>
-      <Route exact path="/" component={Home} />
-      <Route path="/search" component={Search} />
-      <Route path="/playlist/:id" component={Playlist} />
-    </Routes>
-  </Router>
+    <div className="App">
+      <header className="App-header">
+        <div className="SearchContainer">
+          <h2>Searchly</h2>
+          {!token ? (
+            <div>
+              <a
+                href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+              >
+                Login to Spotify
+              </a>
+            </div>
+          ) : (
+            <div>
+              <Searcher token={token} />
+              <button className="logOut" onClick={logout}>
+                Logout
+              </button>
+            </div>
+          )}
+        </div>
+      </header>
+    </div>
   );
 }
 
